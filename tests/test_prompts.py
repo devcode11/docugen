608f870d53be05ea90b44d2478562e0388981c3b
devcode11 <80094366+devcode11@users.noreply.github.com>
2025-03-05 12:17:27 +0530

    Fix mypy errors

--- tests/test_prompts.py
+++ tests/test_prompts.py
@@ -2,21 +2,21 @@ from unittest import TestCase
 from docugen import prompts
 
 class FilePromptTests(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.file_path = 'abc/def/file1.py'
         self.file_contents = 'This is file1 contents'
 
-    def test_prompt_contains_file_path(self):
+    def test_prompt_contains_file_path(self) -> None:
         prompt = prompts.summary_for_file_prompt(file_path=self.file_path, file_contents=self.file_contents)
         self.assertRegex(prompt, fr'\b{self.file_path}\b')
 
-    def test_prompt_contains_file_contents(self):
+    def test_prompt_contains_file_contents(self) -> None:
         prompt = prompts.summary_for_file_prompt(file_path=self.file_path, file_contents=self.file_contents)
         self.assertRegex(prompt, fr'\b{self.file_contents}\b')
 
 
 class DirectoryPromptTests(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.dir_path = 'abc/def/dir1'
         self.submodules = []
         for i in range(5):
@@ -24,16 +24,16 @@ class DirectoryPromptTests(TestCase):
         for i in range(5, 10):
             self.submodules.append((f'{self.dir_path}/subdir{i}', f'Summary of subdir{i}'))
 
-    def test_prompt_contains_directory_path(self):
+    def test_prompt_contains_directory_path(self) -> None:
         prompt = prompts.summary_for_directory_prompt(module_path=self.dir_path, submodules=self.submodules)
         self.assertRegex(prompt, fr'\b{self.dir_path}\b')
 
-    def test_prompt_contains_submodules_paths(self):
+    def test_prompt_contains_submodules_paths(self) -> None:
         prompt = prompts.summary_for_directory_prompt(module_path=self.dir_path, submodules=self.submodules)
         for m in self.submodules:
             self.assertRegex(prompt, fr'\b{m[0]}\b')
 
-    def test_prompt_contains_submodules_summaries(self):
+    def test_prompt_contains_submodules_summaries(self) -> None:
         prompt = prompts.summary_for_directory_prompt(module_path=self.dir_path, submodules=self.submodules)
         for m in self.submodules:
             self.assertRegex(prompt, fr'\b{m[1]}\b')
